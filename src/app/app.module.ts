2f6cae70256a3275a31366cdad2695ed39192cf0
Riadh MNASRI <riadh.mnasri@gmail.com>
2018-07-14 19:33:19 +0200

    Add jobs admin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { JobsComponent } from './admin/jobs/jobs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobsComponent
   ],
   imports: [
     BrowserModule
